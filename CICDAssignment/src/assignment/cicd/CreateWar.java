53f515fd348438a50213e6db15f93b32207415b0
sarmila sahu <sarmilasahu@sarmilas-MacBook-Air.local>
2019-10-27 17:24:05 -0700

    2nd commit

--- CICDAssignment/src/assignment/cicd/CreateWar.java
+++ CICDAssignment/src/assignment/cicd/CreateWar.java
@@ -2,7 +2,7 @@ package assignment.cicd;
 
 public class CreateWar {
 	public static void main(String[] args) {
-		System.out.println("Sample WAR file generation");
+		System.out.println("Sample WAR file generation!!!!");
 	}
 
 }
